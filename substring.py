0d5058b1b2e68d3282bd90020d9892991ad946f0
Eeswaruday <157198060+Eeswaruday@users.noreply.github.com>
2025-09-08 09:47:58 +0530

    substring.py

---
+++ substring.py
@@ -0,0 +1,6 @@
+string=input("Enter the main string:")
+substring=input("Enter the sub string:")
+if substring in string:
+    print("substring is present.")
+else:
+    print("substring is not present.")
